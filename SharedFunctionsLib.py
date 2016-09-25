d93f13224c8a3bba60e179d30ebac7bf82d0c620
justin <jdano0914@gmail.com>
2016-09-25 14:58:06 -0400

    Added database call for setting start date Included timing (1998-now) took 559 seconds

--- SharedFunctionsLib.py
+++ SharedFunctionsLib.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import MySQLdb as mdb
 
+
 def get_db_connection():
-	db_host = 'localhost'
-	db_user = 'root'
-	db_pass = 'GoldfishSmiles.com'
-	db_name = 'Securities_master3'
-	return mdb.connect(db_host, db_user, db_pass, db_name)
+    db_host = 'localhost'
+    db_user = 'root'
+    db_pass = 'GoldfishSmiles.com'
+    db_name = 'securities_master'
+    return mdb.connect(db_host, db_user, db_pass, db_name)
+
 
 def retrieve_db_tickers(connection):
     """
@@ -23,101 +25,109 @@ def retrieve_db_tickers(connection):
 
 
 def retrieve_price_data(con, ticker, vendor_id, start, end):
-	"""
-	Retrive price data from the database for the specific
-	ticker over the specified range.
-	:param ticker: The company's ticker symbol
-	:param vendor_id: The vendor we grab the price data from
-	:param start: We grab data starting at this date
-	:param end: The data ends at this date
-	:return:Dataframe of all the price data
-	"""
-	with con:
-		cur = con.cursor()
-		# First query is to retrieve the id for the given symbol
-		cur.execute("SELECT id FROM symbol WHERE ticker = %s", [ticker])
-		ticker_id = cur.fetchall()
-		ticker_id = ticker_id[0][0]
-		# Second query gathers OHLCAV data
-		cur.execute("SELECT * FROM daily_price WHERE \
+    """
+    Retrive price data from the database for the specific
+    ticker over the specified range.
+    :param ticker: The company's ticker symbol
+    :param vendor_id: The vendor we grab the price data from
+    :param start: We grab data starting at this date
+    :param end: The data ends at this date
+    :return:Dataframe of all the price data
+    """
+    with con:
+        cur = con.cursor()
+        # First query is to retrieve the id for the given symbol
+        cur.execute("SELECT id FROM symbol WHERE ticker = %s", [ticker])
+        ticker_id = cur.fetchall()
+        ticker_id = ticker_id[0][0]
+        # Second query gathers OHLCAV data
+        cur.execute("SELECT * FROM daily_price WHERE \
 		symbol_id = %s AND data_vendor_id = %s \
 		AND price_date BETWEEN %s AND %s", (ticker_id, vendor_id, start, end))
 
-		# Return none if no data exists for the ticker within the specified
-		# time frame, otherwise return a dataframe with the OHLCAV data
-		if not cur.rowcount:
-			if vendor_id == 1:
-				print "Yahoo had no price data available for " + str(start)
-			else:
-				print "Quandl had no price data available for " + str(start)
-			price_data = None
-		else:
-			price_data = pd.DataFrame(list(cur.fetchall()))
-			price_data.columns = ['id', 'symbol_id', 'vendor_id', 'price_date', 'created_date',
-			                      'last_updated_date', 'open_price', 'high_price', 'low_price', 'close_price',
-			                      'adj_close_price', 'volume']
+        # Return none if no data exists for the ticker within the specified
+        # time frame, otherwise return a dataframe with the OHLCAV data
+        if not cur.rowcount:
+            if vendor_id == 1:
+                print "Yahoo had no price data available for " + str(start)
+            else:
+                print "Quandl had no price data available for " + str(start)
+            price_data = None
+        else:
+            price_data = pd.DataFrame(list(cur.fetchall()))
+            price_data.columns = ['id', 'symbol_id', 'vendor_id', 'price_date', 'created_date',
+                                  'last_updated_date', 'open_price', 'high_price', 'low_price', 'close_price',
+                                  'adj_close_price', 'volume']
 
-		return price_data
+        return price_data
+
 
 def retrieve_data_with_zeros(con, ticker, start, end):
-	"""
+    """
 
-	:param con:
-	:param ticker:
-	:param start:
-	:param end:
-	:return:
-	"""
-	with con:
-		cur = con.cursor()
-		# First query is to retrieve the id for the given symbol
-		cur.execute("SELECT id FROM symbol WHERE ticker = %s", [ticker])
-		ticker_id = cur.fetchall()
-		ticker_id = ticker_id[0][0]
-		# Second query gathers OHLCAV data
-		#cur.execute("SELECT * FROM clean_prices WHERE \
-		#symbol_id = %s AND price_date BETWEEN %s AND %s", (ticker_id, start, end))
-		cur.execute("SELECT * FROM clean_prices2 WHERE \
+    :param con:
+    :param ticker:
+    :param start:
+    :param end:
+    :return:
+    """
+    with con:
+        cur = con.cursor()
+        # First query is to retrieve the id for the given symbol
+        cur.execute("SELECT id FROM symbol WHERE ticker = %s", [ticker])
+        ticker_id = cur.fetchall()
+        ticker_id = ticker_id[0][0]
+        # Second query gathers OHLCAV data
+        # cur.execute("SELECT * FROM clean_prices WHERE \
+        # symbol_id = %s AND price_date BETWEEN %s AND %s", (ticker_id, start, end))
+        cur.execute("SELECT * FROM clean_prices2 WHERE \
 		symbol_id = %s AND price_date >= %s AND price_date <= %s", (ticker_id, start, end))
-		# Return none if no data exists for the ticker within the specified
-		# time frame, otherwise return a dataframe with the OHLCAV data
-		if not cur.rowcount:
-			print "No price data available for " + ticker + " on " + str(start)
-			price_data = None
-		else:
-			price_data = pd.DataFrame(list(cur.fetchall()))
-			price_data.columns = ['id', 'symbol_id', 'price_date', 'created_date',
-			                      'last_updated_date', 'open_price', 'high_price', 'low_price', 'close_price',
-			                      'adj_close_price', 'volume']
+        # Return none if no data exists for the ticker within the specified
+        # time frame, otherwise return a dataframe with the OHLCAV data
+        if not cur.rowcount:
+            print "No price data available for " + ticker + " on " + str(start)
+            price_data = None
+        else:
+            price_data = pd.DataFrame(list(cur.fetchall()))
+            price_data.columns = ['id', 'symbol_id', 'price_date', 'created_date',
+                                  'last_updated_date', 'open_price', 'high_price', 'low_price', 'close_price',
+                                  'adj_close_price', 'volume']
 
-		return price_data
+        return price_data
+
 
 def insert_data_into_db(con, price_data):
-	"""
-	Insert Dataframe into SQL database
-	:param price_data: OHLCAV data
-	"""
-	#print "about to insert into price_data into db:"
-	#print price_data
-	#price_data['symbol_id'] = ticker
-	#print "new price_data"
-	#print price_data
-	#sys.exit()
-	with con:
-		price_data.to_sql(con=con, name='clean_prices3', if_exists='append', index=False, flavor='mysql')
+    """
+    Insert Dataframe into SQL database
+    :param price_data: OHLCAV data
+    """
+    # print "about to insert into price_data into db:"
+    # print price_data
+    # price_data['symbol_id'] = ticker
+    # print "new price_data"
+    # print price_data
+    # sys.exit()
+    with con:
+        price_data.to_sql(con=con, name='clean_prices3', if_exists='append', index=False, flavor='mysql')
 
+
+"""
 def delete_duplicates(con, table, symbol_id, price_date):
-	"""
 
-	:param con:
-	:param table:
-	:param symbol_id:
-	:param price_date:
-	:return:
-	"""
 
-	with con:
-		cur = con.cursor()
-		# First query is to retrieve the id for the given symbol
-		cur.execute("SELECT id FROM symbol WHERE ticker = %s", [ticker])
-		cur.execute("DELETE %s FROM %s INNER JOIN (select")
+    with con:
+        cur = con.cursor()
+        # First query is to retrieve the id for the given symbol
+        # cur.execute("SELECT id FROM symbol WHERE ticker = %s", [ticker])
+        cur.execute("DELETE %s FROM %s INNER JOIN (select")
+
+"""
+
+
+def assign_starting_date(con, ticker, first_day):
+    query = """ UPDATE symbol SET first_price_date = %s WHERE ticker = %s """
+    data = (first_day, ticker)
+
+    with con:
+        cur = con.cursor()
+        cur.execute(query, data)
